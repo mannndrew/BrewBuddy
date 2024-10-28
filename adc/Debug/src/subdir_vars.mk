11845faea8bb3bb44f1beb2b35374e973fa0897b
mannndrew <mannndrew@gmail.com>
2024-10-27 23:11:28 -0500

    Added adc example

---
+++ adc/Debug/src/subdir_vars.mk
@@ -0,0 +1,50 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/adc0.c \
+../src/clock.c \
+../src/main.c \
+../src/uart0.c \
+../src/wait.c 
+
+C_DEPS += \
+./src/adc0.d \
+./src/clock.d \
+./src/main.d \
+./src/uart0.d \
+./src/wait.d 
+
+OBJS += \
+./src/adc0.obj \
+./src/clock.obj \
+./src/main.obj \
+./src/uart0.obj \
+./src/wait.obj 
+
+OBJS__QUOTED += \
+"src\adc0.obj" \
+"src\clock.obj" \
+"src\main.obj" \
+"src\uart0.obj" \
+"src\wait.obj" 
+
+C_DEPS__QUOTED += \
+"src\adc0.d" \
+"src\clock.d" \
+"src\main.d" \
+"src\uart0.d" \
+"src\wait.d" 
+
+C_SRCS__QUOTED += \
+"../src/adc0.c" \
+"../src/clock.c" \
+"../src/main.c" \
+"../src/uart0.c" \
+"../src/wait.c" 
+
+
